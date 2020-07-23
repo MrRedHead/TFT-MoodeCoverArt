9d28a1df4d1e4f94096000801c3ee9baceb7c38f
MrRedHead <mr__redhead@hotmail.com>
2020-07-23 21:02:06 +0200

    Add files via upload
    Add a line under the title with encoded format

--- tft_moode_coverart.py
+++ tft_moode_coverart.py
@@ -8,7 +8,7 @@ from os import path
 import RPi.GPIO as GPIO
 from mediafile import MediaFile
 from io import BytesIO
-from numpy import mean 
+from numpy import mean
 import ST7789
 from PIL import ImageFilter
 import yaml
@@ -19,7 +19,7 @@ __version__ = "0.0.4"
 
 # get the path of the script
 script_path = os.path.dirname(os.path.abspath( __file__ ))
-# set script path as current directory - 
+# set script path as current directory -
 os.chdir(script_path)
 
 MODE=0
@@ -31,7 +31,7 @@ confile = 'config.yml'
 
 # Read config.yml for user config
 if path.exists(confile):
- 
+
     with open(confile) as config_file:
         data = yaml.load(config_file, Loader=yaml.FullLoader)
         displayConf = data['display']
@@ -42,26 +42,26 @@ if path.exists(confile):
 
 
 
-     
+
 # Standard SPI connections for ST7789
 # Create ST7789 LCD display class.
-if MODE == 3:    
+if MODE == 3:
     disp = ST7789.ST7789(
         port=0,
         cs=ST7789.BG_SPI_CS_FRONT,  # GPIO 8, Physical pin 24
         dc=9,
         rst=22,
-        backlight=13,               
+        backlight=13,
         mode=3,
         rotation=0,
         spi_speed_hz=80 * 1000 * 1000
-    )   
-else:   
+    )
+else:
     disp = ST7789.ST7789(
         port=0,
-        cs=ST7789.BG_SPI_CS_FRONT,  # GPIO 8, Physical pin 24 
+        cs=ST7789.BG_SPI_CS_FRONT,  # GPIO 8, Physical pin 24
         dc=9,
-        backlight=13,               
+        backlight=13,
         spi_speed_hz=80 * 1000 * 1000
     )
 
@@ -126,7 +126,7 @@ def isServiceActive(service):
 def getMoodeMetadata(filename):
     # Initalise dictionary
     metaDict = {}
-    
+
     if path.exists(filename):
         # add each line fo a list removing newline
         nowplayingmeta = [line.rstrip('\n') for line in open(filename)]
@@ -136,7 +136,7 @@ def getMoodeMetadata(filename):
             (key, value) = nowplayingmeta[i].split('=')
             metaDict[key] = value
             i += 1
-        
+
         metaDict['source'] = 'library'
         if 'file' in metaDict:
             if (metaDict['file'].find('http://', 0) > -1) or (metaDict['file'].find('https://', 0) > -1):
@@ -156,8 +156,8 @@ def getMoodeMetadata(filename):
             elif metaDict['file'].find('Squeezelite Active', 0) > -1:
                 metaDict['source'] = 'squeeze'
             elif metaDict['file'].find('Input Active', 0) > -1:
-                metaDict['source'] = 'input' 
-            
+                metaDict['source'] = 'input'
+
 
     # return metadata
     return metaDict
@@ -167,7 +167,7 @@ def get_cover(metaDict):
     cover = None
     cover = Image.open(script_path + '/images/default-cover-v6.jpg')
     covers = ['Cover.jpg', 'cover.jpg', 'Cover.jpeg', 'cover.jpeg', 'Cover.png', 'cover.png', 'Cover.tif', 'cover.tif', 'Cover.tiff', 'cover.tiff',
-		'Folder.jpg', 'folder.jpg', 'Folder.jpeg', 'folder.jpeg', 'Folder.png', 'folder.png', 'Folder.tif', 'folder.tif', 'Folder.tiff', 'folder.tiff']
+                'Folder.jpg', 'folder.jpg', 'Folder.jpeg', 'folder.jpeg', 'Folder.png', 'folder.png', 'Folder.tif', 'folder.tif', 'Folder.tiff', 'folder.tiff']
     if metaDict['source'] == 'radio':
         if 'coverurl' in metaDict:
             rc = '/var/www/' + metaDict['coverurl']
@@ -189,15 +189,15 @@ def get_cover(metaDict):
         if 'file' in metaDict:
             if len(metaDict['file']) > 0:
 
-                fp = '/var/lib/mpd/music/' + metaDict['file']   
-                mf = MediaFile(fp)     
+                fp = '/var/lib/mpd/music/' + metaDict['file']
+                mf = MediaFile(fp)
                 if mf.art:
                     cover = Image.open(BytesIO(mf.art))
                     return cover
                 else:
                     for it in covers:
                         cp = os.path.dirname(fp) + '/' + it
-                        
+
                         if path.exists(cp):
                             cover = Image.open(cp)
                             return cover
@@ -207,7 +207,7 @@ def get_cover(metaDict):
 def main():
 
     disp.set_backlight(True)
-    
+
     filename = '/var/local/www/currentsong.txt'
 
     c = 0
@@ -218,6 +218,7 @@ def main():
     x1 = 20
     x2 = 20
     x3 = 20
+    x4 = 20
     title_top = 105
     volume_top = 184
     time_top = 222
@@ -226,11 +227,11 @@ def main():
     if act_mpd == True:
         while True:
             client = musicpd.MPDClient()   # create client object
-            try:     
+            try:
                 client.connect()           # use MPD_HOST/MPD_PORT
             except:
                 pass
-            else:                  
+            else:
                 moode_meta = getMoodeMetadata(filename)
 
                 mpd_current = client.currentsong()
@@ -238,13 +239,13 @@ def main():
                 cover = get_cover(moode_meta)
 
 
-                
+
                 mn = 50
                 if OVERLAY == 3:
                     img.paste(cover.resize((WIDTH,HEIGHT), Image.LANCZOS).convert('RGB'))
                 else:
                     img.paste(cover.resize((WIDTH,HEIGHT), Image.LANCZOS).filter(ImageFilter.GaussianBlur).convert('RGB'))
-                
+
                 if 'state' in mpd_status:
                     if (mpd_status['state'] == 'stop') and (BLANK != 0):
                         if ss < BLANK:
@@ -254,12 +255,12 @@ def main():
                     else:
                         ss = 0
                         disp.set_backlight(True)
-                
-                
-                im_stat = ImageStat.Stat(cover) 
+
+
+                im_stat = ImageStat.Stat(cover)
                 im_mean = im_stat.mean
                 mn = mean(im_mean)
-                
+
                 #txt_col = (255-int(im_mean[0]), 255-int(im_mean[1]), 255-int(im_mean[2]))
                 txt_col = (255,255,255)
                 bar_col = (255, 255, 255, 255)
@@ -270,7 +271,7 @@ def main():
                     bar_col = (100,100,100,225)
                 if mn < 80:
                     txt_col = (200,200,200)
-                
+
                 if (moode_meta['source'] == 'library') or (moode_meta['source'] == 'radio'):
 
                     if (OVERLAY > 0) and (OVERLAY < 3):
@@ -291,17 +292,17 @@ def main():
                                     img.paste(vol_icons, (0,0), vol_icons)
                                 else:
                                     img.paste(vol_icons_dark, (0,0), vol_icons_dark)
-                            
+
                         else:
                             img.paste(play_icons, (0,0), play_icons)
-    
+
                         if 'volume' in mpd_status:
                             vol = int(mpd_status['volume'])
                             vol_x = int((vol/100)*(WIDTH - 33))
                             draw.rectangle((5, volume_top, WIDTH-34, volume_top+8), (255,255,255,145))
                             draw.rectangle((5, volume_top, vol_x, volume_top+8), bar_col)
-                    
-                    if OVERLAY < 3:    
+
+                    if OVERLAY < 3:
                         if TIMEBAR == 1:
                             if 'elapsed' in  mpd_status:
                                 el_time = int(float(mpd_status['elapsed']))
@@ -310,8 +311,8 @@ def main():
                                     dur_x = int((el_time/du_time)*(WIDTH-10))
                                     draw.rectangle((5, time_top, WIDTH-5, time_top + 12), (255,255,255,145))
                                     draw.rectangle((5, time_top, dur_x, time_top + 12), bar_col)
-        
-                        
+
+
                         top = 7
                         if 'artist' in moode_meta:
                             w1, y1 = draw.textsize(moode_meta['artist'], font_m)
@@ -321,9 +322,9 @@ def main():
                             if w1 <= WIDTH:
                                 x1 = (WIDTH - w1)//2
                             draw.text((x1, top), moode_meta['artist'], font=font_m, fill=txt_col)
-                        
+
                         top = 35
-                        
+
                         if 'album' in moode_meta:
                             w2, y2 = draw.textsize(moode_meta['album'], font_s)
                             x2 = x2-20
@@ -333,7 +334,7 @@ def main():
                                 x2 = (WIDTH - w2)//2
                             draw.text((x2, top), moode_meta['album'], font=font_s, fill=txt_col)
 
-                        
+
                         if 'title' in moode_meta:
                             w3, y3 = draw.textsize(moode_meta['title'], font_l)
                             x3 = x3-20
@@ -343,6 +344,15 @@ def main():
                                 x3 = (WIDTH - w3)//2
                             draw.text((x3, title_top), moode_meta['title'], font=font_l, fill=txt_col)
 
+                        if 'encoded' in moode_meta:
+                            w4, y4 = draw.textsize(moode_meta['encoded'], font_s)
+                            x4 = x4-20
+                            if x4 < (WIDTH - w4 - 20):
+                                x4 = 0
+                            if w4 <= WIDTH:
+                                x4 = (WIDTH - w4)//2
+                            draw.text((x4, title_top+30), moode_meta['encoded'], font=font_s, fill=txt_col)
+
 
                 else:
                     if 'file' in moode_meta:
@@ -351,8 +361,8 @@ def main():
                         x3 = (WIDTH - w3)//2
                         y3 = (HEIGHT - h3)//2
                         draw.text((x3, y3), txt, font=font_l, fill=txt_col, spacing=6, align="center")
-            
-            
+
+
             disp.display(img)
 
             if c == 0:
@@ -370,7 +380,7 @@ def main():
         mlw, mlh = draw.multiline_textsize(txt, font=font_m, spacing=4)
         draw.multiline_text(((WIDTH-mlw)//2, 20), txt, fill=(255,255,255), font=font_m, spacing=4, align="center")
         disp.display(img)
-        
+
 
 
 
